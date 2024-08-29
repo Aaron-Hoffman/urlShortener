883ecb63f8199e692314b2deb86c055fb2dbd726
Aaron <aaron@Aarons-MacBook-Pro.local>
2024-08-29 12:06:02 -0400

    Set up short url service

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+const dns = require('node:dns');
+
+const urlData = {};
+let currentNumber = 0;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +24,23 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', function(req, res) {
+  dns.lookup(req.body.url, (err, address, family) => {
+    if (!address) {
+      res.json({ error: 'invalid url' });
+    } else {
+      currentNumber++;
+      urlData[currentNumber] = req.body.url;
+      res.json({ original_url : req.body.url, short_url : currentNumber});
+    }
+  })
+})
+
+app.get('/api/shorturl/:id', function(req, res) {
+  let redirectPath = urlData[req.params.id];
+  res.redirect(`http://${redirectPath}`);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
